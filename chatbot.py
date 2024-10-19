d6234b0d04b36669882f974dd1d6c5b2da448727
sowmyabalasubramanian <sowmya.connect@gmail.com>
2024-10-19 12:31:15 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,4 +16,4 @@ class Chatbot:
       self.messages.append({"role": "user", "content": user_message})
       ai_message = llm_groq.invoke(self.messages)
       self.messages.append({"role": "assistant", "content": ai_message.content})
-      return ai_message.content + "\n" + "Tokens used: " + str(ai_message.response_metadata["token_usage"]["total_tokens"])
+      return ai_message.content + "\n" + "Total Tokens used: " + str(ai_message.response_metadata["token_usage"]["total_tokens"])
